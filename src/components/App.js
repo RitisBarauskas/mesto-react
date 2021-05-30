0eb49b97bf3159b12fff5f524622cf20e4a68522
Ritis <jimmyelf@gmail.com>
2021-05-31 00:08:20 +0500

    Update App

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ImagePopup from './ImagePopup';
 
 
 function App() {
-    const [selectedCard, setSelectedCard] = React.useState(false);
+    const [selectedCard, setSelectedCard] = React.useState({name: '', link: ''});
     const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
     const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
